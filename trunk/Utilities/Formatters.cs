9c83cedb0fd1c144edb3e48273ea9576f7ac837c
ebouwsema <ebouwsema@4479b240-e449-11de-b184-cf21a9d0b08a>
2009-12-09 04:26:03 +0000

    Added Utilities

---
+++ trunk/Utilities/Formatters.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Langevin.Core.Utilities
+{
+    public class Formatters
+    {
+        public static string FormatPhoneNumber(string phoneNumber)
+        {
+            phoneNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+            phoneNumber = phoneNumber.TrimStart('1');
+            return string.Format("({0}) {1}-{2}", phoneNumber.Substring(0, 3), phoneNumber.Substring(3, 3), phoneNumber.Substring(6, 4));
+        }
+
+    }
+}

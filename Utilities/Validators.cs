1ad47fbe0f3f4cd7ad70f5721d6b65ec504da98e
ebouwsema <ebouwsema@4479b240-e449-11de-b184-cf21a9d0b08a>
2009-12-09 04:26:03 +0000

    Added Utilities

---
+++ Utilities/Validators.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Langevin.Core.Utilities
+{
+    public static class Validators
+    {
+        public static bool ValidatePhoneNumber(string phoneNumber) {
+            phoneNumber = Regex.Replace(phoneNumber, @"[^\d]", "");
+            phoneNumber = phoneNumber.TrimStart('1');
+            if (phoneNumber.Length != 10)
+                return false;
+            return true;
+        }
+
+        public static bool ValidateEmail(string email)
+        {
+            return Regex.IsMatch(email, @"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}", RegexOptions.IgnoreCase);
+        }
+    }
+}
